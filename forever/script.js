954eab05d340682a83d1042d3f8cb9c8ea43489f
edynprojects <onasanyaemmanuel088@gmail.com>
2023-01-24 19:07:06 +0100

    updates

--- forever/script.js
+++ forever/script.js
@@ -9,3 +9,25 @@ else if(time<12) {
 else{
     greeting.innerHTML = " Hey! Good Day!";
 }
+
+var menuBtn = document.getElementById("menuBtn");
+var sideNav = document.getElementById("sideNav");
+var menu = document.getElementById("menu");
+sideNav.style.right="-250px";
+// sideNav.style.right="-250px";
+// menu.onclick = function() {
+//     menu.src= "./img/close.png ";
+// }
+
+menuBtn.addEventListener("click", function() {
+    if (sideNav.style.right=="-250px"){
+        sideNav.style.right= "100px";
+        menu.src= "./img/close.png ";
+    }
+    else {
+        sideNav.style.right="-250px";
+        menu.src= "./img/icons8-menu-30.png";
+    
+    }
+});
+menuBtn.onclick = function() 
