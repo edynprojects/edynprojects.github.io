019c68aea8df798cc12e00978fb066e26a10352b
edynprojects <onasanyaemmanuel088@gmail.com>
2023-06-04 15:47:58 +0100

    update

---
+++ cotnew/script.js
@@ -0,0 +1,20 @@
+// JAVASCRIPT CODE 
+
+//  HAMBURGER MENU 
+let menu = document.querySelector(".menu")
+let nav = document.querySelector(".nav")
+let navmenu = document.querySelector(".navmenu")
+
+menu.addEventListener("click", () => {
+    
+    if (nav.style.top=="100%"){
+        nav.style.top= "0px";                
+        menu.src= "./img/close.png ";
+    }
+    else {
+        nav.style.top="100%";
+        menu.src= "./img/icons8-menu-30.png";
+    
+    }
+}
+)
