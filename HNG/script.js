80097b2725b9847550b952b7d568b111b9249dc7
edynprojects <121686908+edynprojects@users.noreply.github.com>
2024-07-02 12:50:31 +0100

    push

--- HNG/script.js
+++ HNG/script.js
@@ -1,13 +1,13 @@
-function timeAndDay() {
-   const currentTImeElement = document.querySelector('.currentTimeUTC')
-   const currentDayElement = document.querySelector('.currentDay')
+// function timeAndDay() {
+//    const currentTImeElement = document.querySelector('.currentTimeUTC')
+//    const currentDayElement = document.querySelector('.currentDay')
 
-   const presentTme = new Date()
-   const utcTime = presentTme.toUTCString().split('  ')[4]
-   const dayOfWeek = presentTme.toLocaleDateString('en-US', { weekday:"long"})
-   currentDayElement.textContent  = dayOfWeek
-   currentTImeElement.textContent  = utcTime
-}
-timeAndDay()
+//    const presentTme = new Date()
+//    const utcTime = presentTme.toUTCString().split('  ')[4]
+//    const dayOfWeek = presentTme.toLocaleDateString('en-US', { weekday:"long"})
+//    currentDayElement.textContent  = dayOfWeek
+//    currentTImeElement.textContent  = utcTime
+// }
+// timeAndDay()
 
-console.log("omooo")
+// console.log("omooo")
