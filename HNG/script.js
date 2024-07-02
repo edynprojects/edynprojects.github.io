af2871a9a3b0631c8faf675d21972f69c005951b
edynprojects <121686908+edynprojects@users.noreply.github.com>
2024-07-03 00:22:24 +0100

    Testing

--- HNG/script.js
+++ HNG/script.js
@@ -1,13 +1,15 @@
-// function timeAndDay() {
-//    const currentTImeElement = document.querySelector('.currentTimeUTC')
-//    const currentDayElement = document.querySelector('.currentDay')
+function timeAndDay() {
+   const currentTImeElement = document.querySelector('[data-testid="currentTimeUTC"]')
+   const currentDayElement = document.querySelector('.currentDay')
 
-//    const presentTme = new Date()
-//    const utcTime = presentTme.toUTCString().split('  ')[4]
-//    const dayOfWeek = presentTme.toLocaleDateString('en-US', { weekday:"long"})
-//    currentDayElement.textContent  = dayOfWeek
-//    currentTImeElement.textContent  = utcTime
-// }
-// timeAndDay()
+   const presentTime = new Date()
+   const utcTime = presentTime.toUTCString().split(' ')[4]
+   const dayOfWeek = presentTime.toLocaleDateString('en-US', { weekday:"long"})
+   currentDayElement.textContent  = dayOfWeek
+   currentTImeElement.textContent  = utcTime
+}
+timeAndDay()
 
-// console.log("omooo")
+setInterval(timeAndDay, 1000);
+
+console.log("omooo")
